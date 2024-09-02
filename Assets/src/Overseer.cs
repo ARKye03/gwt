1b22673bf4e734b471e4afcbef89f00fdee5e636
ARKye03 <rafa03-dev@proton.me>
2024-09-01 22:57:42 -0400

    Remove GameOverseer.cs and add Overseer.cs

---
+++ Assets/src/Overseer.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+/*
+    * Why overseer?
+    * 1 - Is the final boss of the first map of my favorite game
+    * 2 - I can
+    * 3 - I want
+    * 4 - I will
+    * 5 - I am the overseer!
+*/
+
+// This file could be called Board.cs but I'm retarded!
+
+public class Board : MonoBehaviour
+{
+    int RoundsPlayed = 0;
+    bool KeepPlaying = true;
+
+    public Lib.Faction WhoIsPlaying = Lib.Faction.Idanai;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
