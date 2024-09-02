6086ecdd3790021316db6b41473eb441bbd41851
ARKye03 <rafa03-dev@proton.me>
2024-09-01 22:59:39 -0400

    Knowledge is power

--- Assets/src/Decks/CardsQuanto.cs
+++ Assets/src/Decks/CardsQuanto.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class CardsQuanto : MonoBehaviour
 {
+    // needs to be a singleton <= idk wtf is this
+    // Edit:
+    /*
+     * Singleton is a creational design pattern that lets you ensure that a class has only one instance, while providing a global access point to this instance.
+     */
     List<Card> CardsOfIdanai = new()
     {
     };
