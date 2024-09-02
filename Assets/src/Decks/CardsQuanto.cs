1d9a27dd1c676a5fa0c6440904d9fd9d640e63b0
ARKye03 <rafa03-dev@proton.me>
2024-09-01 22:58:53 -0400

    Meant to the future

---
+++ Assets/src/Decks/CardsQuanto.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardsQuanto : MonoBehaviour
+{
+    List<Card> CardsOfIdanai = new()
+    {
+    };
+    List<Card> CardsOfCelai = new()
+    {
+    };
+    List<Card> CardsOfYudivain = new()
+    {
+    };
+}
