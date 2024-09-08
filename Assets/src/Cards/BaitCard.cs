a8f4cd9b2e5064bab32b86a9f2de88a19eec7f6c
ARKye03 <rafa03-dev@proton.me>
2024-09-08 12:39:58 -0400

    Refactor BaitCard class and add new properties and methods

--- Assets/src/Cards/BaitCard.cs
+++ Assets/src/Cards/BaitCard.cs
@@ -1,9 +1,29 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewBaitCard", menuName = "Card/BaitCard")]
 public class BaitCard : Card
 {
-    public BaitCard(string name,
-                    string description,
-                    TypeofCard typeOfCard,
-                    Faction faction,
-                    Effect effect,
-                    bool isVoidCard = false) : base() { }
+    public float initialDmg = 0;
+    public float dmg = 0;
+
+    public float GetCurrentDMG => dmg;
+    public float ModDmg { get => dmg; set => dmg = value; }
+
+    public void Initialize(string name,
+                           string description,
+                           TypeofCard typeOfCard,
+                           Faction faction,
+                           float initialDmg,
+                           Effect effect,
+                           bool isVoidCard = false)
+    {
+        this.name = name;
+        this.initialDmg = initialDmg;
+        Description = description;
+        TypeofCard = typeOfCard;
+        Faction = faction;
+        dmg = initialDmg;
+        Effect = effect;
+        IsVoidCard = isVoidCard;
+    }
 }
