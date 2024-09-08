628d59dadae4f0999a5026ffd0425831b0fe237e
ARKye03 <rafa03-dev@proton.me>
2024-09-08 14:41:56 -0400

    Refactor ClimateCard class to remove unused variables and properties

--- Assets/src/Cards/ClimateCard.cs
+++ Assets/src/Cards/ClimateCard.cs
@@ -5,26 +5,17 @@ public class ClimateCard : Card
 {
     public RowType AffectedRow;
     public int StrengthReduction;
-    public float initialDmg = 0;
-    public float dmg = 0;
-
-    public float GetCurrentDMG => dmg;
-    public float ModDmg { get => dmg; set => dmg = value; }
-
     public void Initialize(string name,
                            string description,
                            TypeofCard typeOfCard,
                            Faction faction,
-                           float initialDmg,
                            Effect effect,
                            RowType affectedRow)
     {
         this.name = name;
-        this.initialDmg = initialDmg;
         Description = description;
         TypeofCard = typeOfCard;
         Faction = faction;
-        dmg = initialDmg;
         Effect = effect;
         AffectedRow = affectedRow;
     }
