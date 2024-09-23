6a2c565672a5fe038817f04f91d58f5524bbdb5d
ARKye03 <rafa03-dev@proton.me>
2024-09-22 20:24:40 -0400

    docs(Tooltips): Documented tooltips

--- Assets/src/Tooltip/Tooltip.cs
+++ Assets/src/Tooltip/Tooltip.cs
@@ -1,15 +1,31 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
 
+/// <summary>
+/// The Tooltip class is responsible for displaying and hiding tooltips when the pointer enters or exits the UI element.
+/// /// </summary>
 public class Tooltip : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
+    /// <summary>
+    /// The message to be displayed in the tooltip.
+    /// </summary>
     public string msg;
-
+    /// /// 
+    /// <summary>
+    /// Called when the pointer enters the UI element.
+    /// Displays the tooltip with the specified message.
+    /// </summary>
+    /// <param name="eventData">Current event data.</param>
     public void OnPointerEnter(PointerEventData eventData)
     {
         TooltipManager._instance.ShowTooltip(msg);
     }
 
+    /// <summary>
+    /// Called when the pointer exits the UI element.
+    /// Hides the tooltip.
+    /// </summary>
+    /// <param name="eventData">Current event data.</param>
     public void OnPointerExit(PointerEventData eventData)
     {
         TooltipManager._instance.HideTooltip();
--- Assets/src/Tooltip/TooltipManager.cs
+++ Assets/src/Tooltip/TooltipManager.cs
@@ -1,12 +1,24 @@
-using System.Collections;
-using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
+/// <summary>
+/// Manages the display of tooltips in the game.
+/// </summary>
 public class TooltipManager : MonoBehaviour
 {
+    /// <summary>
+    /// Singleton instance of the TooltipManager.
+    /// </summary>
     public static TooltipManager _instance;
+
+    /// <summary>
+    /// Reference to the TextMeshProUGUI component used to display the tooltip text.
+    /// </summary>
     public TextMeshProUGUI textUI;
+
+    /// <summary>
+    /// Ensures that only one instance of TooltipManager exists.
+    /// </summary>
     void Awake()
     {
         if (_instance != null && _instance != this)
@@ -18,19 +30,36 @@ public class TooltipManager : MonoBehaviour
             _instance = this;
         }
     }
+
+    /// <summary>
+    /// Hides the tooltip on start.
+    /// </summary>
     void Start()
     {
         gameObject.SetActive(false);
     }
+
+    /// <summary>
+    /// Updates the position of the tooltip to follow the mouse cursor.
+    /// </summary>
     void Update()
     {
         transform.position = Input.mousePosition;
     }
+
+    /// <summary>
+    /// Displays the tooltip with the specified message.
+    /// </summary>
+    /// <param name="msg">The message to display in the tooltip.</param>
     public void ShowTooltip(string msg)
     {
         gameObject.SetActive(true);
         textUI.text = msg;
     }
+
+    /// <summary>
+    /// Hides the tooltip and clears the text.
+    /// </summary>
     public void HideTooltip()
     {
         gameObject.SetActive(false);
