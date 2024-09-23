302eb3be952bf1424d1a31edd2f8e3623dd7d244
ARKye03 <rafa03-dev@proton.me>
2024-09-22 20:38:35 -0400

    refactor: Overseer and tootip docs

--- Assets/src/Tooltip/Tooltip.cs
+++ Assets/src/Tooltip/Tooltip.cs
@@ -3,14 +3,13 @@ using UnityEngine.EventSystems;
 
 /// <summary>
 /// The Tooltip class is responsible for displaying and hiding tooltips when the pointer enters or exits the UI element.
-/// /// </summary>
+/// </summary>
 public class Tooltip : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     /// <summary>
     /// The message to be displayed in the tooltip.
     /// </summary>
     public string msg;
-    /// /// 
     /// <summary>
     /// Called when the pointer enters the UI element.
     /// Displays the tooltip with the specified message.
