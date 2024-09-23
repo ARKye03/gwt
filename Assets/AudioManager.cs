6d17935c1f886f2799076b30f9a4d76554aeb175
ARKye03 <rafa03-dev@proton.me>
2024-09-22 22:26:57 -0400

    docs: AudioManager & ButtonFX

--- Assets/AudioManager.cs
+++ Assets/AudioManager.cs
@@ -1,9 +1,14 @@
 using UnityEngine;
 
+/// <summary>
+/// The AudioManager class is responsible for managing audio within the game.
+/// It ensures that the audio manager object persists across different scenes.
+/// </summary>
 public class AudioManager : MonoBehaviour
 {
     void Awake()
     {
+        // Simplest method to persist object through scenes
         DontDestroyOnLoad(gameObject);
     }
 }
--- Assets/ButtonFX.cs
+++ Assets/ButtonFX.cs
@@ -1,19 +1,38 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
 
+/// <summary>
+/// The ButtonFX class is responsible for playing a sound effect when a pointer enters the button area.
+/// Implements the <see cref="IPointerEnterHandler"/> interface to handle pointer enter events.
+/// </summary>
 public class ButtonFX : MonoBehaviour, IPointerEnterHandler
 {
+    /// <summary>
+    /// The AudioSource component used to play the audio clip.
+    /// </summary>
     public AudioSource audioSource;
+
+    /// <summary>
+    /// The AudioClip to be played when the pointer enters the button area.
+    /// </summary>
     public AudioClip audioClip;
 
+    /// <summary>
+    /// Plays the assigned audio clip using the AudioSource component.
+    /// This method is called when the pointer enters the button area.
+    /// </summary>
     public void PlaySoundOnHover()
     {
         audioSource.PlayOneShot(audioClip);
     }
 
+    /// <summary>
+    /// Called when the pointer enters the button area.
+    /// Triggers the PlaySoundOnHover method.
+    /// </summary>
+    /// <param name="eventData">Current event data.</param>
     public void OnPointerEnter(PointerEventData eventData)
     {
         PlaySoundOnHover();
     }
-
 }
