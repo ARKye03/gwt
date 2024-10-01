044fc7b16ad91b739ed42aaca4c924d776fa6c53
ARKye03 <rafa03-dev@proton.me>
2024-10-01 10:54:54 -0400

    feat(PuaseMenu): Add not that simple reloadScene function and button

--- Assets/src/PauseMenu.cs
+++ Assets/src/PauseMenu.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
@@ -15,6 +16,29 @@ public class PauseMenu : MonoBehaviour
         {
             _instance = this;
         }
+    }
+    public void ReloadScene()
+    // A - Can this be more simple?  
+    // B - I guess we'll never know.
+    {
+        var board = Board._instance;
+        var cardsQuanto = CardsQuanto._instance;
+
+        if (board == null || cardsQuanto == null)
+        {
+            Debug.LogError("Board or CardsQuanto instance is null");
+            return;
+        }
+
+        // Clear and reinitialize cards
+        cardsQuanto.Clear();
+        cardsQuanto.InitializeCards();
+
+        // Reinitialize decks
+        board.decks = board.InitDecks(cardsQuanto);
+
+        // Reload the current scene
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
     public void QuitGame()
     {
